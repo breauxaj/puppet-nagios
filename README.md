5afae05c6643bd7e3a2c09832a5b42e61012a12b
Adam Breaux <breauxaj@gmail.com>
2013-09-20 11:17:38 -0400

    split nagios and nrpe

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 nagios
 ======
 
-Installs nrpe with the main plugins. Use nagios::server to install the server
-and configure monitoring.
+Installs nagios with the main plugins.
 
 Samples
 -------
@@ -11,43 +10,4 @@ include nagios
 ```
 ```
 nagios::service { 'default': ensure => running, enable => true }
-```
-```
-nagios::config { 'default':
-  server_port => 5666,
-  server_address => '',
-  allowed_hosts => '192.168.1.55',
-  command_timeout => 60,
-  connection_timeout = 300
-}
-```
-```
-nagios::disks { 'default':
-  warning => 20,
-  critical => 10
-}
-```
-```
-nagios::load { 'default':
-  warning => '15,10,5',
-  critical => '30,25,20'
-}
-```
-```
-nagios::procs { 'default':
-  warning => 150,
-  critical => 200
-}
-```
-```
-nagios::users { 'default':
-  warning => 5,
-  critical => 10
-}
-```
-
-Server
-------
-```
-include nagios::server
 ```
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,51 +1,13 @@
 class nagios {
+  Class['nagios']->Class['apache']
+
   $required = $::operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'nrpe', 'nagios-plugins-all' ],
+    /(?i-mx:centos|fedora|redhat|scientific)/ => [
+      'nagios',
+      'nagios-plugins-all'
+    ],
   }
 
   package { $required: ensure => latest }
-
-  file { '/etc/nrpe.d':
-    ensure  => directory,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-    recurse => true,
-    force   => true,
-    purge   => true,
-  }
-
-  file { '/var/run/nrpe':
-    ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0775',
-  }
-
-  file { '/etc/nagios/nrpe.cfg':
-    ensure => absent,
-  }
-
-  file { '/etc/init.d/nrpe':
-    ensure => present,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => 'puppet:///modules/nagios/nrpe.init'
-  }
-
-  group { 'nrpe':
-    ensure => present,
-    gid    => 126,
-  }
-
-  user { 'nrpe':
-    ensure     => present,
-    gid        => 126,
-    home       => '/var/lib/nrpe',
-    shell      => '/sbin/nologin',
-    managehome => true,
-    uid        => 126,
-  }
 
 }
