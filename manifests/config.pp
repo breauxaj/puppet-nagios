122eaf6308a370116f241f5fcfb6540600e6b5c2
Adam Breaux <breauxaj@gmail.com>
2013-09-27 15:05:28 -0400

    Match rpm install settings

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,11 +7,17 @@ define nagios::config (
 
   file { '/etc/nagios/nagios.cfg':
     ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0664',
     notify => Service[$service],
   }
 
   file { '/etc/nagios/cgi.cfg':
     ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
     notify => Service[$service],
   }
 
--- manifests/init.pp
+++ manifests/init.pp
@@ -5,9 +5,14 @@ class nagios {
     /(?i-mx:centos|fedora|redhat|scientific)/ => 'nagios',
   }
 
+  $conf = $::operatingsystem ? {
+    /(?i-mx:centos|fedora|redhat|scientific)/ => [
+      '/etc/nagios'
+    ],
+  }
+
   $paths = $::operatingsystem ? {
     /(?i-mx:centos|fedora|redhat|scientific)/ => [
-      '/etc/nagios',
       '/etc/nagios/conf.d',
       '/etc/nagios/objects',
       '/etc/nagios/private'
@@ -16,11 +21,18 @@ class nagios {
 
   package { $required: ensure => latest }
 
-  file { $paths:
+  file { $conf:
     ensure => directory,
     owner  => 'root',
     group  => 'root',
-    mode   => '0755',
+    mode   => '0775',
+  }
+
+  file { $paths:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'nagios',
+    mode   => '0750',
   }
 
 }
