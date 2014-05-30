1acc9a17017c2955af1ceda09b3f0a4bf00c2d84
Adam Breaux <breauxaj@gmail.com>
2014-05-30 14:27:19 -0400

    add to list

--- manifests/plugins.pp
+++ manifests/plugins.pp
@@ -1,6 +1,9 @@
 class nagios::plugins {
   $required = $::operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => 'nagios-plugins-all',
+    /(?i-mx:centos|fedora|redhat|scientific)/ => [
+      'nagios-plugins-all',
+      'nagios-plugins-check-updates'
+    ]
   }
 
   package { $required: ensure => latest }
