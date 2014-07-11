0d38036dc868ebb78687e80fd59a29131274f624
Adam Breaux <breauxaj@gmail.com>
2014-07-11 07:50:17 -0400

    oops

--- manifests/pnp4nagios.pp
+++ manifests/pnp4nagios.pp
@@ -2,7 +2,7 @@ class nagios::pnp4nagios (
   $ensure = 'latest'
 ){
   $required = $::operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => 'php4nagios'
+    /(?i-mx:centos|fedora|redhat|scientific)/ => 'pnp4nagios'
   }
 
   package { $required: ensure => $ensure }
